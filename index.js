c9019035765e77ffcb72a97cfd81361fc7f54cf5
Augusto Parizio <augustoparizio.eng@gmail.com>
2021-03-25 13:20:58 -0300

    campanhaVacinação

--- index.js
+++ index.js
@@ -1,74 +1,73 @@
 let nomePetshop = "PETSHOP AVANADE";
 
-let pets =[
+let pets = [
     {
-        nome: 'Apollo',
+        nome: 'luna',
         tipo: 'cachorro',
-        idade: 3,
-        raca: 'pitbull',
-        peso: 30,
-        tutor: 'Augusto',
+        idade: 2,
+        raca: 'Vira-lata',
+        peso: 10,
+        tutor: 'Edson',
         vacinado: false,
         servicos: ['banho', 'vermifugação']
     },
     {
-        nome: 'J',
+        nome: 'foguete',
         tipo: 'cachorro',
-        idade: 2,
-        raca: 'hotweiler',
-        peso: 36,
-        tutor: 'Pedro',
+        idade: 1,
+        raca: 'São bernardo',
+        peso: 30,
+        tutor: 'Luana',
         vacinado: false,
         servicos: ['banho', 'tosa']
     },
     {
-        nome: 'Jubileu',
+        nome: 'Bob',
         tipo: 'gato',
         idade: 3,
-        raca: 'vira-lata',
+        raca: 'cianes',
         peso: 2,
-        tutor: 'Alice',
+        tutor: 'barbara',
         vacinado: false,
         servicos: ['banho', 'vacinação']
     },
 ];
 
 // 
-
+const exibirPet = (pet) => {
+    console.log(`Nome: ${pet.nome}
+    Idade: ${pet.idade}
+    Tipo: ${pet.tipo}
+    Raca: ${pet.raca}
+    Vacinado: ${pet.vacinado}`)
+}
 const listarPets = () => {
-
+   
     for(let pet of pets){
         exibirPet(pet)
     }
 }
 
 const vacinarPet = (pet) => {
-
+    
     if(pet.vacinado == false){
         pet.vacinado = true
+        exibirPet(pet)
+        return 1
     }
-
-    exibirPet(pet)
-
-
-  
-}
-const exibirPet = (pet) => {
-    console.log( console.log(`Nome do Cachorro: ${pet.nome}
-    Idade: ${pet.idade}
-    Tipo: ${pet.tipo}
-    Raca: ${pet.raca}
-    Vacinado: ${pet.vacinado}`))
 }
 
-
+const campanhaVacinacao = (pet) => {
+    let vacinados = 0
+    for (pet of pets){
+        vacinados += vacinarPet(pet)
+    }
+    console.log(vacinados)
+}
 
 
 
 //listarPets();
-vacinarPet(pets[1])
-   
-
-
-
+//vacinarPet(pets[2])
+campanhaVacinacao()
 
